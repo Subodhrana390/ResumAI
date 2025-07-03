cb0bc62db0985f5087f85ecdd5f21bc8e561d6ce
Subodh Rana <subodhrana390@gmail.com>
2025-07-03 16:17:54 +0000

    fix scalingproper viewing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function LandingPage() {
                 width="600"
                 height="400"
                 alt="ResumAI Interface"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square shadow-xl"
+                className="mx-auto aspect-[3/2] overflow-hidden rounded-xl object-cover sm:w-full lg:order-last shadow-xl"
               />
             </div>
           </div>
